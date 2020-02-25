712a8c0d1f8918e14221579b05fc2df18dc0125b
Your Name <you@example.com>
2020-02-25 17:37:40 +0800

    codes for sz clsf

--- Plotting/Plot_FC/lc_netplot.m
+++ Plotting/Plot_FC/lc_netplot.m
@@ -1,4 +1,4 @@
-function lc_netplot(net_path,if_add_mask,mask_path,how_disp,if_binary,which_group, net_index_path)
+function lc_netplot(net_path,if_add_mask,mask_path,how_disp,if_binary,which_group,net_index_path, is_legend, legends)
 % Use: plot matrix with sorted according to network order
 % input
 %   net_path:带路径的功能连接网络文件名
@@ -69,41 +69,39 @@ net_index=importdata(net_index_path);
 [index,re_net_index,re_net]=lc_ReorganizeNetForYeo17NetAtlas(net,net_index);
 
 % plot: insert separate line between each network
-lc_InsertSepLineToNet(re_net, re_net_index, 0.4);
+lc_InsertSepLineToNet(re_net, re_net_index, 0.4, is_legend, legends);
 % axis square
 end
 
-function lc_InsertSepLineToNet(net, network_index, linewidth, location_of_sep)
-% 此代码的功能：在一个网络矩阵种插入网络分割线，此分割线将不同的脑网络分开
+function lc_InsertSepLineToNet(net, network_index, linewidth, is_legend, legends)
+% 此代码的功能：在一个网络矩阵种插入网络分割线，以及bar
+% 此分割线将不同的脑网络分开
+% 不同颜色的区域，代表一个不同的网络
 % input
 %   net:一个网络矩阵，N*N,N为节点个数，必须为对称矩阵
 %   network_index: network index of each node.
 %   location_of_sep:分割线的index，为一个向量，比如[3,9]表示网络分割线分别位于3和9后面
 %% input
-if nargin < 4
-    % if not given location_of_sep, then generate it using network_index;
-    uni_id = unique(network_index);
-    location_of_sep = [0; cell2mat(arrayfun( @(id) max(find(network_index == id)), uni_id, 'UniformOutput',false))];
-%     sepIndex=importdata('D:\WorkStation_2018\WorkStation_dynamicFC_V2\Data\Network_and_plot_para\sepIndex.mat');
-end
+% if not given location_of_sep, then generate it using network_index;
+uni_id = unique(network_index);
+location_of_sep = [0; cell2mat(arrayfun( @(id) max(find(network_index == id)), uni_id, 'UniformOutput',false))];
 
 if size(net,1)~=size(net,2)
     error('Not a symmetric matrix!\n');
 end
 
 %%
-% figure
-% figure;
-imagesc(net)
-% insert separate line
-lc_line(location_of_sep,length(net),linewidth)
-axis off
-end
+n_node = length(net);
+imagesc(net);
 
-function lc_line(sepIndex, nNode, linewidth)
-% nNode: node个数
-for i=1:length(sepIndex)
-    line([sepIndex(i)+0.5,sepIndex(i)+0.5],[-10,nNode*2],'color','k','LineWidth',linewidth)
-    line([0,nNode*2],[sepIndex(i)+0.5,sepIndex(i)+0.5],'color','k','LineWidth',linewidth)
-end
+% insert separate line
+lc_line(location_of_sep, n_node, linewidth);
+hold on;
+
+% bar region
+extend = n_node / 10;
+xlim([-extend, n_node]);
+ylim([0, n_node + extend]);
+lc_bar_region_of_each_network(location_of_sep, n_node, extend, is_legend, legends);
+axis off
 end
