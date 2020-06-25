602b0ca8369673fd3503897648be0fa4dedbca9e
LiChao <lichao19870617@gmail.com>
2020-06-25 11:14:38 +0800

    Update lc_get_individual_state_and_metrics.m

--- Workstation/code_workstation2018_dynamicFC/clustering/lc_get_individual_state_and_metrics.m
+++ Workstation/code_workstation2018_dynamicFC/clustering/lc_get_individual_state_and_metrics.m
@@ -1,36 +1,67 @@
-function lc_get_individual_state_and_metrics(idx,k,dir_of_dFC,all_subjname,out_dir)
+function lc_get_individual_state_and_metrics(varargin)
 % PURPOSE:  To get subject's centroid and state metrics of each state according group centroid (idx)
 % Each subject's dynamic FC data is nNode*nNode*nWindow tensor
 % NOTE: Not all subjects have all state, but all subjects have state metrics.
-% Inputs:
-    % k: number of clusters
-    % dir_of_dFC: folder containing the dFC (Producing a tensor with dimension nNode*nNode*nWindow*nSubj after loading)
-    % ordered_subjname: ordered subject names; the order of idx must match the ordered_subjname
-    % out_dir: directory to save the results
-% Outputs:
+% INPUTS:
+%   [--idx_file, -idf]: Cluster indices
+%   [--subname, -sn]: Ordered subject names which are used to match idx and dfc.
+%   [--cluster_number, -k]: number of clusters
+%   [--dir_of_dFC, -dod]: folder containing the dFC (Producing a tensor with dimension nNode*nNode*nWindow*nSubj after loading)
+%   [--ordered_subjname, -os]: ordered subject names; the order of idx must match the ordered_subjname
+%   [--out_dir, -od]: directory to save the results
+% OUTPUTS:
     % Saving all subjects' centroid networks to out_dir
     % subname_of_each_state: subjects' names of each state group used to extract covariates such as age, sex, headmotion etc.
-%% input
-if nargin < 1  % TODO
-    idx_path = fullfile('D:\WorkStation_2018\WorkStation_dynamicFC_V3\Data\results\results_dfc_heldOutSamples','idx.mat');
-    k = 2;
-    dir_of_dFC = 'D:\WorkStation_2018\WorkStation_dynamicFC_V3\Data\results\results_dfc_heldOutSamples\zDynamicFC_WindowLength17_WindowStep1';
-    subname = 'D:\WorkStation_2018\WorkStation_dynamicFC_V3\Data\results\results_dfc_heldOutSamples\ordered_subjname_2020111154955.txt';
-    out_dir = 'D:\WorkStation_2018\WorkStation_dynamicFC_V3\Data\results\results_dfc_heldOutSamples\results_of_individual';
-    idx = importdata(idx_path);
-    all_subjname = importdata(subname);
+% 
+% EXAMPLE:
+% Input LC_GET_INDIVIDUAL_STATE_AND_METRICS without any varargin will interactively run. 
+
+%% ---------------------------VARARGIN PARSER-------------------------------
+if( sum(or(strcmpi(varargin,'--idx_file'),strcmpi(varargin,'-idf')))==1)
+    idx_file = varargin{find(or(strcmpi(varargin,'--idx_file'),strcmp(varargin,'-idf')))+1};
+else
+    [idx_name, idx_path] = uigetfile({'*.mat';'*.*'},'Select cluster indices');  
+    idx_file = fullfile(idx_path,idx_name);
 end
+idx = importdata(idx_file);
+
+if( sum(or(strcmpi(varargin,'--subname'),strcmpi(varargin,'-sn')))==1)
+    subname = varargin{find(or(strcmpi(varargin,'--subname'),strcmp(varargin,'-sn')))+1};
+else
+    [sn, sub_path] = uigetfile({'*.txt';'*.*'},'Select ordered subjects names');  
+    subname = fullfile(sub_path,sn);
+    subname = importdata(subname);
+end
+
+if( sum(or(strcmpi(varargin,'--cluster_number'),strcmpi(varargin,'-k')))==1)
+    cluster_number = varargin{find(or(strcmpi(varargin,'--cluster_number'),strcmp(varargin,'-k')))+1};
+else
+    cluster_number = input('Enter cluster_number:');
+end
+
+if( sum(or(strcmpi(varargin,'--dir_of_dFC'),strcmpi(varargin,'-dod')))==1)
+    dir_of_dFC = varargin{find(or(strcmpi(varargin,'--dir_of_dFC'),strcmp(varargin,'-dod')))+1};
+else
+    dir_of_dFC = uigetdir(pwd, 'Select directory of DFC');
+end
+
+if( sum(or(strcmpi(varargin,'--out_dir'),strcmpi(varargin,'-od')))==1)
+    out_dir = varargin{find(or(strcmpi(varargin,'--out_dir'),strcmp(varargin,'-od')))+1};
+else
+    out_dir = uigetdir(pwd, 'Select directory for saving results');
+end
+%% ---------------------------END VARARGIN PARSER-------------------------------
 
 %%
 % Make dir for saving individual states
-for i=1:k
+for i=1:cluster_number
     if ~exist(fullfile(out_dir,['individual_state',num2str(i)]),'dir')
         mkdir(fullfile(out_dir,['individual_state',num2str(i)]));
     end
 end
 
 % check input
-n_subj = length(all_subjname);
+n_subj = length(subname);
 [n_row,~] = size(idx);
 if fix(n_row/n_subj) ~= n_row/n_subj
     fprintf('Number of subjects'' name and number of rows are mismatched\n');
@@ -45,14 +76,14 @@ dFCName={dfc_file.name}';
 dfc_file=fullfile(dir_of_dFC,dFCName);
 
 % Get each subject's median network
-n_subj = length(all_subjname);
+n_subj = length(subname);
 ind_start = 1:num_window:n_row;
 ind_end = num_window:num_window:n_row;
 
 for ithSubj = 1:n_subj
     fprintf('%d/%d\n',ithSubj,n_subj);
-    subjname = all_subjname{ithSubj};
-    get_median_network(idx, k, ithSubj,ind_start,ind_end,dfc_file,subjname, out_dir);
+    subjname = subname{ithSubj};
+    get_median_network(idx, cluster_number, ithSubj,ind_start,ind_end,dfc_file,subjname, out_dir);
 end
 fprintf('------------------------All Done!------------------------\n');
 end
